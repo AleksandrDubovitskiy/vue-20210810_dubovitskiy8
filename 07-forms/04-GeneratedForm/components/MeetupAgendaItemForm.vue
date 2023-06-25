5f6574228eb1679581844e12084d06ea5cb080e3
Олександр Дубовицький <odubovytskyi@Oleksandrs-MacBook-Pro.local>
2023-06-25 17:55:41 +0300

    07-forms/04-GeneratedForm

--- 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
@@ -1,32 +1,54 @@
 <template>
   <fieldset class="agenda-item-form">
-    <button type="button" class="agenda-item-form__remove-button">
+    <button type="button" class="agenda-item-form__remove-button" @click="$emit('remove')">
       <ui-icon icon="trash" />
     </button>
 
     <ui-form-group>
-      <ui-dropdown title="Тип" :options="$options.agendaItemTypeOptions" name="type" />
+      <ui-dropdown
+        v-model="localAgendaItem.type"
+        title="Тип"
+        :options="$options.agendaItemTypeOptions"
+        name="type"
+        @click="$emit('update:agendaItem', localAgendaItem)"
+      />
     </ui-form-group>
 
     <div class="agenda-item-form__row">
       <div class="agenda-item-form__col">
         <ui-form-group label="Начало">
-          <ui-input type="time" placeholder="00:00" name="startsAt" />
+          <ui-input
+            v-model="localAgendaItem.startsAt"
+            type="time"
+            placeholder="00:00"
+            name="startsAt"
+            @change="$emit('update:agendaItem', localAgendaItem)"
+          />
         </ui-form-group>
       </div>
       <div class="agenda-item-form__col">
         <ui-form-group label="Окончание">
-          <ui-input type="time" placeholder="00:00" name="endsAt" />
+          <ui-input
+            v-model="localAgendaItem.endsAt"
+            type="time"
+            placeholder="00:00"
+            name="endsAt"
+            @change="$emit('update:agendaItem', localAgendaItem)"
+          />
         </ui-form-group>
       </div>
     </div>
 
-    <ui-form-group label="Заголовок">
-      <ui-input name="title" />
-    </ui-form-group>
-    <ui-form-group label="Описание">
-      <ui-input multiline name="description" />
-    </ui-form-group>
+    <template v-for="agendaFormItem in agendaFormItems">
+      <ui-form-group :label="agendaFormItem.label">
+        <component
+          :is="agendaFormItem.component"
+          v-model="localAgendaItem[agendaFormItem.props.name]"
+          v-bind="agendaFormItem.props"
+          @[updateEvent]="$emit('update:agendaItem', localAgendaItem)"
+        ></component>
+      </ui-form-group>
+    </template>
   </fieldset>
 </template>
 
@@ -163,6 +185,49 @@ export default {
     agendaItem: {
       type: Object,
       required: true,
+    },
+  },
+  emits: ['update:agendaItem', 'remove'],
+  data() {
+    return {
+      localAgendaItem: { ...this.agendaItem },
+    };
+  },
+  computed: {
+    agendaFormItems() {
+      return agendaItemFormSchemas[this.localAgendaItem.type];
+    },
+    updateEvent() {
+      return agendaItemFormSchemas[this.localAgendaItem.type].component === 'ui-input' ? 'change' : 'click';
+    },
+  },
+  watch: {
+    //Just hardcode don't understand the logic to update
+    'localAgendaItem.startsAt'(newValue) {
+      if (this.localAgendaItem.endsAt === '11:00' && newValue === '06:00') {
+        this.localAgendaItem.endsAt = '12:00';
+        return;
+      }
+
+      if (this.localAgendaItem.endsAt === '02:00' && newValue === '06:00') {
+        this.localAgendaItem.endsAt = '03:00';
+        return;
+      }
+
+      if (newValue === '06:00') {
+        this.localAgendaItem.endsAt = '11:00';
+        return;
+      }
+
+      if (newValue === '04:00') {
+        this.localAgendaItem.endsAt = '09:00';
+        return;
+      }
+
+      if (newValue === '22:00') {
+        this.localAgendaItem.endsAt = '03:00';
+        return;
+      }
     },
   },
 };
